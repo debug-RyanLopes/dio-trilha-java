0d27eb7557dc689a5775e8e2dee0490a1dd625db
Ryan <ryanoliveiralopes@gmail.com>
2025-06-25 00:21:06 -0400

    Entrega do Projeto simples de verificação e deposito em um banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        Scanner sc =  new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+
+        //TODO: Perguntar as informações bancarias do usuário
+        System.out.println("Qual o seu nome? ");
+        var nome = sc.nextLine(); 
+
+        System.out.println("Qual o numero da sua conta?");
+        var numeroConta = sc.nextInt();
+
+        System.out.println("Qual o numero da sua agencia? (somente numeros, sem pontuação)");
+        var numeroAgencia = sc.nextInt();
+
+        System.out.println("E qual seria o valor a ser depositado? ");
+        var saldo = sc.nextDouble();
+
+
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo R$%s já está disponível para saque", nome, numeroAgencia, numeroConta, saldo );
+
+
+
+        sc.close();
     }
 }
